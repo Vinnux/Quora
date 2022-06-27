b0ab03ac49fa5f15e74da5c17b59cd33481b5c18
VirginieBroye <v.broye@free.fr>
2022-06-27 11:54:27 +0200

    affichage de la liste des questions

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,31 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+
+        $questions = [
+            [
+                'title' => 'Je suis une question',
+                'content' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque magnam incidunt eveniet neque et veniam! Minus assumenda minima, voluptates ut deleniti sint odio maxime blanditiis perferendis autem tenetur harum quaerat!',
+                'rating' => 20,
+                'author' => [
+                    'name' => 'Virginie B',
+                    'avatar' => "https://randomuser.me/api/portraits/lego/6.jpg"
+                ],
+                'nbResponse' => 15
+            ],
+            [
+                'title' => 'Je suis une deuxieme question',
+                'content' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque magnam incidunt eveniet neque et veniam! Minus assumenda minima, voluptates ut deleniti sint odio maxime blanditiis perferendis autem tenetur harum quaerat!',
+                'rating' => 18,
+                'author' => [
+                    'name' => 'Julie V',
+                    'avatar' => "https://randomuser.me/api/portraits/women/81.jpg"
+                ],
+                'nbResponse' => 6
+            ]
+        ];
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'questions' => $questions,
         ]);
     }
 }
--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -12,7 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class QuestionController extends AbstractController
 {
     #[Route('/question/ask', name: 'question_form')]
-    public function index(Request $request): Response
+    public function ask(Request $request): Response
     {
         $formQuestion = $this->createForm(QuestionType::class);
         $formQuestion->handleRequest($request);
