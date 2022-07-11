67531c7603ad0ed36da2b9766432878a6b10541b
VirginieBroye <v.broye@free.fr>
2022-07-11 11:21:12 +0200

    Creation de la page questions

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\QuestionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,31 +10,33 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(QuestionRepository $questionRepository): Response
     {
-
-        $questions = [
-            [
-                'title' => 'Je suis une question',
-                'content' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque magnam incidunt eveniet neque et veniam! Minus assumenda minima, voluptates ut deleniti sint odio maxime blanditiis perferendis autem tenetur harum quaerat!',
-                'rating' => 20,
-                'author' => [
-                    'name' => 'Virginie B',
-                    'avatar' => "https://randomuser.me/api/portraits/lego/6.jpg"
-                ],
-                'nbResponse' => 15
-            ],
-            [
-                'title' => 'Je suis une deuxieme question',
-                'content' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque magnam incidunt eveniet neque et veniam! Minus assumenda minima, voluptates ut deleniti sint odio maxime blanditiis perferendis autem tenetur harum quaerat!',
-                'rating' => 18,
-                'author' => [
-                    'name' => 'Julie V',
-                    'avatar' => "https://randomuser.me/api/portraits/women/81.jpg"
-                ],
-                'nbResponse' => 6
-            ]
-        ];
+        $questions = $questionRepository->findAll();
+        // $questions = [
+        //     [
+        //         'id' => 1,
+        //         'title' => 'Je suis une question',
+        //         'content' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque magnam incidunt eveniet neque et veniam! Minus assumenda minima, voluptates ut deleniti sint odio maxime blanditiis perferendis autem tenetur harum quaerat!',
+        //         'rating' => 4,
+        //         'author' => [
+        //             'name' => 'Virginie B',
+        //             'avatar' => "https://randomuser.me/api/portraits/lego/6.jpg"
+        //         ],
+        //         'nbResponse' => 15
+        //     ],
+        //     [
+        //         'id' => 2,
+        //         'title' => 'Je suis une deuxieme question',
+        //         'content' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque magnam incidunt eveniet neque et veniam! Minus assumenda minima, voluptates ut deleniti sint odio maxime blanditiis perferendis autem tenetur harum quaerat!',
+        //         'rating' => 18,
+        //         'author' => [
+        //             'name' => 'Julie V',
+        //             'avatar' => "https://randomuser.me/api/portraits/women/81.jpg"
+        //         ],
+        //         'nbResponse' => 6
+        //     ]
+        // ];
         return $this->render('home/index.html.twig', [
             'questions' => $questions,
         ]);
--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Question;
 use App\Form\QuestionType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,16 +14,32 @@ use Symfony\Component\Routing\Annotation\Route;
 class QuestionController extends AbstractController
 {
     #[Route('/question/ask', name: 'question_form')]
-    public function ask(Request $request): Response
+    public function ask(Request $request, EntityManagerInterface $em): Response
     {
-        $formQuestion = $this->createForm(QuestionType::class);
+        $question = new Question();
+        $formQuestion = $this->createForm(QuestionType::class, $question);
         $formQuestion->handleRequest($request);
 
         if($formQuestion->isSubmitted() && $formQuestion->isValid()) {
-            dump($formQuestion->getData());
+            // dump($formQuestion->getData());
+            $question->setNbResponse(0)
+                    ->setRating(0)
+                    ->setCreatedAt(new \DateTimeImmutable());
+
+            $em->persist($question);
+            $em->flush();
+            $this->addFlash('succes', 'Votre question a été ajoutée !');
+            return $this->redirectToRoute('home');
         }
         return $this->render('question/index.html.twig', [
             'form' => $formQuestion->createView(),
         ]);
+    }
+
+
+    #[Route('/question/{id}', name: 'question_show')]
+    public function show(Request $request, Question $question) : Response {
+        
+        return $this->render('question/show.html.twig', ['question' => $question]);
     }
 }
