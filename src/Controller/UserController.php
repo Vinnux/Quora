c47a01ff0303c3a6596ebe08baa5142081cddd80
VirginieBroye <v.broye@free.fr>
2022-07-18 11:05:29 +0200

    Mise en place de la page de l'utilisateur courant

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,14 +3,68 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/user')]
 class UserController extends AbstractController
 {
-    #[Route('/user/{id}', name: 'user')]
+    
+    #[Route('/', name: 'current_user')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function currentUserProfile(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $em) : Response 
+    {
+        /**
+         * @var User
+         */
+        $user = $this->getUser();
+        $userFormProfil = $this->createForm(UserType::class, $user);
+        $userFormProfil->remove('password');
+        $userFormProfil->add('newPassword', PasswordType::class, [
+            'label' => 'Nouveau mot de passe', 
+            'required' => false
+        ]);
+        $userFormProfil->handleRequest($request);
+
+        if($userFormProfil->isSubmitted() && $userFormProfil->isValid()) {
+            $newPassword = $user->getNewPassword();
+
+            if($newPassword) {
+                $hash = $passwordHasher->hashPassword($user, $newPassword);
+                $user->setPassword($hash);
+            }
+
+            $em->flush();
+            $this->addFlash('success', 'Modifications enregistrées !');
+        }
+
+        return $this->render('user/index.html.twig', [
+            'form' => $userFormProfil->createView()
+        ]);
+    }
+
+    #[Route('/questions', name: 'show_questions')]
+    #[IsGranted('IS_AUTHENTICATED_REMEMBERED')]
+    public function showQuestions() : Response 
+    {
+        return $this->render('user/show_questions.html.twig');
+    }
+
+    #[Route('/comments', name: 'show_comments')]
+    #[IsGranted('IS_AUTHENTICATED_REMEMBERED')]
+    public function showComments() : Response 
+    {
+        return $this->render('user/show_comments.html.twig');
+    }
+
+    #[Route('/{id}', name: 'user')]
     #[IsGranted('IS_AUTHENTICATED_REMEMBERED')]
     public function userProfile(User $user): Response
     {
@@ -20,15 +74,6 @@ class UserController extends AbstractController
         }
         return $this->render('user/index.html.twig', [
             'controller_name' => 'Profil d\'un utilisateur',
-        ]);
-    }
-
-    #[Route('/user', name: 'current_user')]
-    #[IsGranted('IS_AUTHENTICATED_FULLY')]
-    public function currentUserProfile() : Response 
-    {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'Mon profil',
         ]);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -31,6 +31,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[Assert\NotBlank(message: "Veuillez entrer un mot de passe")]
     private $password;
 
+    #[Assert\Length(min: 6, minMessage: "Le mot de passe doit faire au minimum 6 carractères.")]
+    private $newPassword;
+
     #[ORM\Column(type: 'string', length: 255)]
     #[Assert\NotBlank(message: "Veuillez entrer votre prénom.")]
     private $firstname;
@@ -113,6 +116,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setPassword(string $password): self
     {
         $this->password = $password;
+
+        return $this;
+    }
+
+    public function getNewPassword(): ?string
+    {
+        return $this->newPassword;
+    }
+
+    public function setNewPassword(string $newPassword): self
+    {
+        $this->newPassword = $newPassword;
 
         return $this;
     }
